fadfee7084ec9f9d9699584854ad98d51b3220af
Fahad Janabi <Fahad.janabi98@yahoo.com>
2022-09-13 12:10:36 -0400

    Methods

---
+++ Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise1
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using MethodsExercise1;
+using System.Drawing;
+
+Console.WriteLine("what is your name");// input
+var userName = Console.ReadLine();
+
+Console.WriteLine($"Hi, {userName}. what is your favorite sport?");// input
+var sport = Console.ReadLine();
+
+Console.WriteLine("what is your favorite food?");//input 
+var food = Console.ReadLine();
+
+Console.WriteLine($"your favorite sport is{sport} your favorite food is{food}");
+
+Console.WriteLine(Methods.Add(10,2));
